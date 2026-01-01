c1685b65aa0d3e1490e72eba666220d28d46be3f
ST <admin@swyftech.com.au>
2026-01-01 14:15:36 +1100

    “Updates”

--- vite.config.ts
+++ vite.config.ts
@@ -54,7 +54,7 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(__dirname, './src'),
         }
       }
     };
